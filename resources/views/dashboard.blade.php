0af7a635604f5f679f35cbca9c07317c6650260d
Gilang Bagus Ramadhan <gilang@polibatam.ac.id>
2024-04-26 10:21:53 +0700

    Admin Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Clarity</title>
+    <title>Admin Dashboard - {{ env('APP_NAME') }}</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
