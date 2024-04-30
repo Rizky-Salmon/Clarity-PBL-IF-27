be46e392b1dabb3493a79dfdd939d36928cad53e
Gilang Bagus Ramadhan <gilang@polibatam.ac.id>
2024-04-30 11:50:39 +0700

    Login - auth layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,126 +1,84 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.auth')
 
-<head>
+@section('title','Login')
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+@section('content')
+<div class="col-xl-10 col-lg-12 col-md-9">
 
-    <title>Login - {{ env('APP_NAME') }}</title>
+    <div class="card o-hidden border-0 shadow-lg my-5" style="background-image: url('img/background_login.png'); background-size: cover;">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Welcome ! Please login to start your session</h1>
+                        </div>
+                        {{-- @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $item)
+                                        <li>{{ $item }}</li>
+                        @endforeach
+                        </ul>
+                    </div>
+                    @endif --}}
+                    <form class="user" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" aria-describedby="emailHelp" value="{{ old('email') }}" placeholder="Enter Email Address..." name="email" autocomplete="email">
+                            @error('email') <small class="invalid-feedback">{{ $message }}</small> @enderror
+                        </div>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" placeholder="Password" name="password">
+                            @error('password') <small class="invalid-feedback">{{ $message }}</small> @enderror
 
-</head>
-
-<body class="bg-gradient-pastel">
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5" style="background-image: url('img/background_login.png'); background-size: cover;">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome ! Please login to start your session</h1>
-                                    </div>
-                                    {{-- @if ($errors->any())
-                                    <div class="alert alert-danger">
-                                        <ul>
-                                            @foreach ($errors->all() as $item)
-                                            <li>{{ $item }}</li>
-                                    @endforeach
-                                    </ul>
-                                </div>
-                                @endif --}}
-                                <form class="user" method="post">
-                                    @csrf
-                                    <div class="form-group">
-                                        <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" aria-describedby="emailHelp" value="{{ old('email') }}" placeholder="Enter Email Address..." name="email" autocomplete="email">
-                                        @error('email') <small class="invalid-feedback">{{ $message }}</small> @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" placeholder="Password" name="password">
-                                        @error('password') <small class="invalid-feedback">{{ $message }}</small> @enderror
-
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" style="color: #9a9cb2;" for="customCheck">Remember
-                                                Me</label>
-                                        </div>
-                                    </div>
-                                    <button name="submit" type="submit" href="" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </button>
-                                    <!-- <hr> -->
-                                    <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a> -->
-                                </form>
-                                <hr>
-                                <div class="text-center">
-                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                </div>
-                                <div class="text-center">
-                                    <a class="small" href="/register">Create an Account!</a>
-                                </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox small">
+                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                <label class="custom-control-label" style="color: #9a9cb2;" for="customCheck">Remember
+                                    Me</label>
                             </div>
                         </div>
-                        <div class="col-lg-6 d-none d-lg-block">
-                            <div class="mx-auto text-center">
-                                <img src="img/clarity_login.png" alt="" style="max-width: 85%; height: auto;">
-
-                            </div>
-                            <div class="mx-auto text-center" style="margin-top: -100px;font-family: 'Alike';
-                                    font-style: normal;
-                                    font-weight: 400;
-                                    font-size: 25px;
-                                    line-height: 38px;
-                                    color: #9a9cb2;
-                                    ">
-                                <p class="small"><b>MAKE YOUR DATA EASY TO SEARCH<br>WITH CLARITY</b></p>
-                            </div>
-                        </div>
+                        <button name="submit" type="submit" href="" class="btn btn-primary btn-user btn-block">
+                            Login
+                        </button>
+                        <!-- <hr> -->
+                        <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <i class="fab fa-google fa-fw"></i> Login with Google
+                                    </a>
+                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    </a> -->
+                    </form>
+                    <hr>
+                    <div class="text-center">
+                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    </div>
+                    <div class="text-center">
+                        <a class="small" href="/register">Create an Account!</a>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-6 d-none d-lg-block">
+                <div class="mx-auto text-center">
+                    <img src="img/clarity_login.png" alt="" style="max-width: 85%; height: auto;">
 
+                </div>
+                <div class="mx-auto text-center" style="margin-top: -100px;font-family: 'Alike';
+                            font-style: normal;
+                            font-weight: 400;
+                            font-size: 25px;
+                            line-height: 38px;
+                            color: #9a9cb2;
+                            ">
+                    <p class="small"><b>MAKE YOUR DATA EASY TO SEARCH<br>WITH CLARITY</b></p>
+                </div>
+            </div>
         </div>
-
     </div>
-
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
+</div>
+</div>
+@endsection
